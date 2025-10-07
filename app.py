84c335187d4791b06343750925cd7822e360cbf1
VihanD <40177098+VihanD@users.noreply.github.com>
2025-10-07 15:16:35 -0400

    Fix Railway 404: bind to 0.0.0.0 and add Procfile

--- app.py
+++ app.py
@@ -191,4 +191,4 @@ def report_fraud():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+        app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
